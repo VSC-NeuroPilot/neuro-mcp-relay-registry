d3f8da7b18b9aac75ac969d5a2290c60142eef9c
KTrain5369 <me@git.ktrain5369.dev>
2025-11-23 22:06:17 +1000

    try to do smth ig

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,29 @@
-export default {
-  fetch(req: Request) {
-    console.log(`[${req.method}] ${req.url}`);
-  },
-};
+import { Hono } from 'hono';
+import { McpServer, ResourceTemplate } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { StreamableHTTPServerTransport } from '@modelcontextprotocol/sdk/server/streamableHttp.js';
+import { z } from 'zod';
+
+// #region MCP Server
+
+const mcp = new McpServer({
+  name: 'neuro-mcp-relay-registry',
+  version: '0.0.1-beta'
+})
+
+// #endregion
+
+// #region Hono app
+
+const app = new Hono();
+
+app.post('/mcp', (c) => {
+  return c.text('Streamable HTTP MCP not yet implemented.')
+})
+
+app.post('/sse', (c) => {
+  return c.text('Legacy SSE not implemented.')
+})
+
+// #endregion
+
+export default app
